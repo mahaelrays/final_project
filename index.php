267e5a641606943d8af798c9c6d44909bcd72172
Aya AbuElkhair <ayaabuelkhair93@yahoo,com>
2015-12-26 01:30:48 +0200

    this is only the design of index page.

---
+++ index.php
@@ -0,0 +1,232 @@
+<?php
+get_header();
+?>
+<body>
+<!--2 inner slider -->
+ <div class="row">
+<div class="col-lg-6">
+<div id="soka-gallery1" class="carousel slide  clear-affix   carousel-1" data-ride="carousel">
+  <div class="carousel-inner  carusalslide" role="listbox">
+    <div class="item imggallery active">
+      <img class="img" src="<?php echo get_template_directory_uri(); ?>/images/panner.jpg" alt="...">      
+    </div>
+    <div class="item  imggallery">
+      <img class="img" src="<?php echo get_template_directory_uri(); ?>/images/slider-2.jpg" alt="...">
+    </div>
+  </div> 
+  </div>
+</div>
+
+<div class="col-lg-5">
+ <div id="soka-gallery2" class="carousel slide  clear-affix  carousel-2" data-ride="carousel">
+  <!-- Indicators -->
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner  carusal-slide" role="listbox">
+    <div class="item img-gallery active">
+    <div class="alter-image" >
+  <h3>While not always necessary, sometimes you need .</h3></br><h3> For those situations, try the panel component.</h3></div>
+    </div>
+    <div class="item  img-gallery">
+   <div class="alter-image" >
+  <h3>While not always necessary, sometimes you need to put your DOM in a box. For those situations, try the panel component.</h3></div>
+    </div>
+  </div> 
+    <a href="" class="cart"><img class="btn btn-succes"  src="<?php echo get_template_directory_uri(); ?>/images/cart.png" alt="..."></a>
+  <div class="subject"><h4>SWEET COCKTAIL DRESSES</h4></div> 
+  <div class="price-offer"><h3 >Price</h3></div> 
+  <div class="arrows">
+   <a class="lefting" href=".carousel"  role="button" data-slide="prev">
+       <img  class="img img-circle" src="<?php echo get_template_directory_uri(); ?>/images/arrow_left.jpg" alt="" />
+    <span class="sr-only">Previous</span>
+  </a>
+   <a class="righting" href=".carousel"   role="button" data-slide="next">
+    <img  class="img img-circle"src="<?php echo get_template_directory_uri(); ?>/images/arrow_right.jpg" alt="" />
+    <span class="sr-only">Next</span>
+  </a>
+  </div>
+  </div> 
+</div>
+ </div>
+
+<div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-xs-3 thumb">
+        <a class="thumbnail" href="#">
+        <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/images/1.PNG"  alt="">
+        <p class="f"><b>Super slim T-shirt</b><br>
+        Adidas</p>
+        <p></p>
+        <p class="f2">$30</p>
+        <p>
+        <button href="#" class="btn btn-lg shop" > <span class="glyphicon glyphicon-shopping-cart" >Shop</span></button> 
+        <button type="button" class="btn btn-default heart-comment"  > <span class="glyphicon glyphicon-heart"></span></button>  
+        <button type="button" class="btn btn-default heart-comment"> <span class="glyphicon glyphicon-comment"></span></button>
+        </p>
+        </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-xs-3 thumb">
+        <a class="thumbnail" href="#">
+        <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/images/2.PNG"  alt="">
+        <p class="f"><b>Slim jeans for girls</b><br>
+        Denim</p>
+        <p></p>
+        <p class="f2">$50</p>
+        <p>
+        <button href="#" class="btn btn-lg shop" > <span class="glyphicon glyphicon-shopping-cart"  >Shop</span></button> 
+        <button type="button" class="btn btn-default heart-comment"  > <span class="glyphicon glyphicon-heart"></span></button>  
+        <button type="button" class="btn btn-default heart-comment"  > <span class="glyphicon glyphicon-comment"></span></button>
+        </p>
+        </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-xs-3 thumb">
+        <a class="thumbnail" href="#">
+        <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/images/3.PNG"  alt="">
+        <p class="f"><b>Casual shoes for men</b><br>
+        Brand here</p>
+        <p></p>
+        <p class="f2">$70</font></p>
+        <p>
+        <button href="#" class="btn btn-lg shop" > <span class="glyphicon glyphicon-shopping-cart">Shop</span></button> 
+        <button type="button" class="btn btn-default heart-comment"  > <span class="glyphicon glyphicon-heart"></span></button>  
+        <button type="button" class="btn btn-default heart-comment"  > <span class="glyphicon glyphicon-comment"></span></button>
+        </p>
+        </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-xs-3 thumb">
+        <a class="thumbnail" href="#">
+        <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/images/4.PNG"  alt="">
+        <p class="f"><b>Lady shoes for men</b><br>
+        David Beckam</p>
+        <p></p>
+        <p class="f2">$80</p>
+        <p>
+        <button href="#" class="btn btn-lg shop" ><span class="glyphicon glyphicon-shopping-cart"  >Shop</span></button> 
+        <button type="button" class="btn btn-default heart-comment"  > <span class="glyphicon glyphicon-heart"></span></button>  
+        <button type="button" class="btn btn-default heart-comment"  > <span class="glyphicon glyphicon-comment"></span></button>
+        </p>
+        </a>
+        </div>
+      </div>  
+      <!-- row1  -->   
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-xs-3  thumb">
+        <a class="thumbnail" href="#">
+        <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/images/5.PNG"  alt="">
+        <p class="f"><b>Golden eveing dress</b><br>
+        Victoria's secret</p>
+        <p></p>
+        <p class="f2">$150</p>
+        <p>
+        <button href="#" class="btn btn-lg shop" > <span class="glyphicon glyphicon-shopping-cart">Shop</span></button> 
+        <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-heart"></span></button>  
+        <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-comment"></span></button>
+        </p>               
+        </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-xs-3  thumb">
+        <a class="thumbnail" href="#">
+        <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/images/6.PNG"  alt="">
+        <p class="f"><b>Casual glasses</b></font><br>
+        Police</p>
+        <p></p>
+        <p class="f2">$15(70%cheaper)</p>
+        <p>
+        <button href="#" class="btn   btn-lg shop" > <span class="glyphicon glyphicon-shopping-cart"  >Shop</span></button> 
+        <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-heart"></span></button>  
+        <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-comment"></span></button>
+        </p>
+        </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-xs-3  thumb">
+        <a class="thumbnail" href="#">
+        <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/images/7.PNG"  alt="">
+        <p class="f"><b>Golden proposal ring</b><br>
+        Coriolan</p>
+        <p></p>
+        <p class="f2">$500</p>
+        <p>
+        <button href="#" class="btn   btn-lg shop" > <span class="glyphicon glyphicon-shopping-cart"  >Shop </span></button> 
+        <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-heart"></span></button>  
+        <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-comment"></span></button>
+        </p>
+        </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-xs-3 thumb">
+        <a class="thumbnail" href="#">
+         <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/images/8.PNG"  alt="">
+         <p class="f"><b>Classic Rolex watch</b><br>
+         <font color="black">Rolex</font></p>
+         <p></p>
+         <p class="f2">$1500</p>
+         <p>
+         <button href="#" class="btn   btn-lg shop" > <span class="glyphicon glyphicon-shopping-cart">Shop</span></button> 
+         <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-heart"></span></button>  
+         <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-comment"></span></button>
+          </p>
+          </a>
+          </div>
+      </div> 
+      <!-- row2 -->
+      <div class="row">
+          <div class="col-lg-3 col-md-3 col-xs-3 thumb">
+          <a class="thumbnail" href="#">
+          <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/images/9.PNG" height="300" width="400" alt="">
+          <p class="f"><b>Cocktail Dress</b><br>
+          Victoria's Secret</p>
+          <p></p>
+          <p class="f2">$65(10%cheaper)</font></p>
+          <p>
+          <button href="#" class="btn   btn-lg shop" > <span class="glyphicon glyphicon-shopping-cart"  >Shop</span></button> 
+          <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-heart"></span></button>  
+          <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-comment"></span></button>
+          </p>
+          </a>
+          </div>
+          <div class="col-lg-3 col-md-3 col-xs-3 thumb">
+          <a class="thumbnail" href="#">
+          <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/images/10.PNG" height="300" width="400" alt="">
+          <p class="f2"><b>Men Bermuda pants</b><br>
+          Nike</p>
+          <p></p>
+          <p class="f2">$45</p>
+          <p>
+          <button href="#" class="btn   btn-lg shop" > <span class="glyphicon glyphicon-shopping-cart"  >Shop</span></button> 
+          <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-heart"></span></button>  
+          <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-comment"></span></button>
+          </p>
+          </a>
+          </div>
+          <div class="col-lg-3 col-md-3 col-xs-3 thumb">
+          <a class="thumbnail" href="#">
+          <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/images/12.PNG" height="300" width="400" alt="">
+          <p class="f"><b>X-mas Golden Socks</b><br>
+          Victoria's Secret</p>
+          <p></p>
+          <p class="f2">$15(60% Cheaper)</p>
+          <p>
+          <button href="#" class="btn   btn-lg shop" > <span class="glyphicon glyphicon-shopping-cart"  >Shop</span></button> 
+          <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-heart"></span></button>  
+          <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-comment"></span></button>
+          </p>
+          </a>
+          </div>
+          <div class="col-lg-3 col-md-3 col-xs-3 thumb">
+          <a class="thumbnail" href="#">
+          <img class="img-responsive" src="<?php echo get_template_directory_uri();?>/images/13.PNG" height="300" width="400" alt="">
+          <p class="f"><b>Business Classic Shirt</b><br>
+          David Beckam</p>
+      <p></p>
+      <p class="f2">$85</p>
+      <p>
+      <button href="#" class="btn   btn-lg shop" > <span class="glyphicon glyphicon-shopping-cart"  >Shop</span></button> 
+      <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-heart"></span></button>  
+      <button type="button" class="btn btn-default    heart-comment"  > <span class="glyphicon glyphicon-comment"></span></button>           
+      </p>
+      </a>
+      </div>
+      </div>
+      <!-- row3 -->
+      <button href="#" class="btn loadmore"> Load more products + </button> 
+      </div>
+<?php
+get_footer();
