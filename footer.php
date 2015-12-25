5c390147a83966266ba65e7602ad679c983a44ba
Aya AbuElkhair <ayaabuelkhair93@yahoo,com>
2015-12-26 00:15:56 +0200

    the footer.

---
+++ footer.php
@@ -0,0 +1,69 @@
+<footer>
+<div class="container-fluid footer">
+<div  class="row1">
+  <div class="col-lg-4  col1">
+     <h4 class="subtitle">About <span class="tcr">SOKA</span></h4>
+    <h5>
+       He says,Monsieur.that his
+      principles won't admit of his
+      drinking
+    </h5>
+     <h5>
+        But that if Monsieur wants to 
+      live another day to drink,then
+      Monsieur had best drop all. 
+
+    </h5>
+    <h5>
+    </br>
+     Boats,and pull the ship away.
+    </h5>
+  </div>
+  <div class="col-lg-4   col2">
+    <h4 class="subtitle">We tweet.<span class="tcr">ALOT</span></h4>
+     <span class="fa fa-twitter fa1"></span>
+    <h5>
+    He says,Mansieur,that his 
+    principles won't work and 
+      don't.
+      </h5>
+     <span class="fa fa-twitter fa2"></span>
+    <h5 class="rr">#Andrwe,Yesterday</h5>
+    <h5>
+     He says,Mansieur,that his 
+     principles won't work.  
+     </h5>
+     <h5 class="rr">#Andrwe,Yesterday</h5>
+  </div>
+  <div class="col-lg-4  col3">
+     <h4 class="subtitle">Here we are.On<span class="tcr"> FLICKR</span></h4>
+   <div class="row">
+   <div class="col-lg-4"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-1.jpg" alt="" /></div>
+   <div class="col-lg-4"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-4.jpg" alt="" /></div>
+   <div class="col-lg-4"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-9.jpg" alt="" /></div>
+   </div>
+   <div class="row">
+   <div class="col-lg-4"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-2.jpg" alt="" /></div>
+   <div class="col-lg-4"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-5.jpg" alt="" /></div>
+   <div class="col-lg-4"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-8.jpg" alt="" /></div>
+   </div>
+   <div class="row">
+   <div class="col-lg-4"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-3.jpg" alt="" /></div>
+   <div class="col-lg-4"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-6.jpg" alt="" /></div>
+   <div class="col-lg-4"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-7.jpg" alt="" /></div>
+   </div>
+  </div>
+</div>
+   <div id="footer" class="col-sm-12">
+      <div class="fdiv col-sm-6">
+        Copyright <span class="glyphicon glyphicon-copyright-mark"></span> 2013 SOKA 2012.All right reserved
+      </div>
+      <div class="fdiv1 col-sm-6">
+        Developed by SOKA Team
+      </div>
+   </div>
+</footer>
+
+<?php wp_footer();?>
+</body>
+</html>
