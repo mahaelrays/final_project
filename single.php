d44d993db0a8a39714a2724ecd55f7c18f10b36e
Aya AbuElkhair <ayaabuelkhair93@yahoo,com>
2015-12-26 02:53:52 +0200

    this is the single page design only.

---
+++ single.php
@@ -0,0 +1,253 @@
+<?php
+get_header();
+?>
+  <body>
+    <div class="container breadcrumb">
+      <ol class="pagination">
+       <li>Home</li>
+       <li>»</li>
+       <li>Women</li>
+       <li>»</li>
+       <li>Dresses</li>
+       <li>»</li>
+       <li>Awesome white shirt and red hat</li>        
+      </ol>
+    </div>
+    <div class="content">
+    <div class="row ">
+         <div class="col-md-1"> </div>
+         <div class="col-md-1 " id="mg"> <img src="<?php echo get_template_directory_uri();?>/images/dreambox.jpg" class="img-thumbnail "></div>
+         <div class="col-md-1">
+         <div class="mg2"><img src="<?php echo get_template_directory_uri();?>/images/imagesk.jpg" width="65" class="img-thumbnail "></div><br>
+                          <img src="<?php echo get_template_directory_uri();?>/images/389793.jpg"  class="img-thumbnail"><br>
+                          <img src="<?php echo get_template_directory_uri();?>/images/imagesnn.jpg"  class="img-thumbnail"><br>
+                          <img src="<?php echo get_template_directory_uri();?>/images/c4.jpg" class="img-thumbnail">
+         </div>
+          <div class="col-lg-6">
+          <hr class="inlinestyle">
+          <h4 ><font color="#606060 "  >Awesome white shirt and red hat</font>
+          <span class="glyphicon glyphicon-star" id="icon"></span>
+          <span class="glyphicon glyphicon-star" id="icon2"></span>
+          <span class="glyphicon glyphicon-star" id="icon2"></span>
+          <span class="glyphicon glyphicon-star" id="icon2"></span>
+          <span class="glyphicon glyphicon-star" id="icon2" ></span></h4><br>
+          <h6 ><font color="#C0C0C0 " >Availability: </font><font color="#ff8067" ><b> YES </b></font> (92 item in stock)</h6>
+          <h6> <font color="#C0C0C0 " >References:</font> PR0000190</h6>
+          <h6> <font color="#C0C0C0 " >Manufacturer: </font>Nicole Miller</h6><br><br>
+        <table >
+          <thead>
+              <tr rowspan="2"  >
+                <th bgcolor="#E8E8E8"  ><a href=""http://localhost/shop/""><font color="#ff8067" size="4"> <div class="text-center"> More info</div></font></a></th>
+                <th ><a href="#"><div class="text-center " id="data"> Data Sheet </div> </a></th>
+                <th  ><a href="#"><div class="text-center"  id="data"> Accessories</div></a> </th>
+                <th  ><a href="#"><div class="text-center" id="data" > Comments</div></a> </th>
+                <th  ><div class="text-center"  > </div> </th>
+              </tr>
+              <tr rowspan="5">
+                <td colspan="6">
+                <div class="text-center" id="paragrf"><p> A good question , a very good question , Hoo-Hoo But we did see-some of them . We had what we called Microscopes and ultramicroscpes, and we put them to our eyes and looked through them , sothat we saw things lager than they really were</p>
+                <p>
+                We had what we called microscopes and ultramicroscpes , and we put them 
+                to our eyes and looked through them , so that we saw things larger than 
+                they really were , and many things we couldnot see .
+                </p>
+                </div>
+                </td>
+                </tr>
+      
+          </thead>
+        </table><br><br>
+        <form>
+           <label>  size : </label>
+           <select >
+            <option value="XS(0-2)" >XS (0-2)</option>
+            <option value="XS(0-3)">XS (0-3)</option>
+            <option value="XS(0-4)">XS (0-4)</option>
+            <option value="XS(0-5)">XS (0-5)</option>
+            <option value="XS(0-6)">XS (0-6)</option>
+            <option value="XS(0-7)">XS (0-7)</option>
+            <option value="XS(0-8)">XS (0-8)</option>
+           </select>
+           <label>  Color : </label>
+           <button id="box" style="width:15px;height:15px;background-color:red;margin-left:2px;">  </button>
+           <select >
+            <option value="Red" >Red</option>
+            <option value="Green" >Green</option>
+            <option value="Blue">Blue</option>
+            <option value="Gray">Gray</option>
+            <option value="Yellow">Yellow</option>
+            <option value="Black">Black</option>
+           </select>
+          <label>  Quantity : </label>
+           <select >
+            <option value="1" >1</option>
+            <option  value="2">2</option>
+            <option  value="3">3</option>
+            <option  value="4">4</option>
+            <option  value="5">5</option>
+           </select> <br>
+           <label><h1 ><font color="#ff8067">  229.29$ </font></h1></label>
+          <button type="button" class="btn btn-warning"><span class="glyphicon glyphicon-shopping-cart"></span>    <b>Buy this dress</b></button>
+        </form>
+        </div>
+        </div>
+        <h3>Related Products</h3>
+        <hr>
+        <div class="row" id="moredress">
+          <div class ="col-md-3">
+              <a target="_blank" href="#">
+              <img src="<?php echo get_template_directory_uri();?>/images/4edf6c70c6be19d.jpg"  class="img-thumbnail ">
+              </a>
+              <div class="desc"><p>Dar Awesome blouse for your awesome body</p></div>
+              <span class="glyphicon glyphicon-star" style="margin-left:100px;"></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ><label style="margin-left:115px;font-size:18px;">$250</label></span>
+          </div>
+          <div class ="col-md-3">
+             <a target="_blank" href="#">
+             <img src="<?php echo get_template_directory_uri();?>/images/img_137.jpg"  class="img-thumbnail ">
+             </a>
+             <div class="desc"><p> Awesome Black dress for nice and  awesome ladies</p></div>
+             <span class="glyphicon glyphicon-star" style="margin-left:100px;"></span>
+             <span class="glyphicon glyphicon-star" ></span>
+             <span class="glyphicon glyphicon-star" ></span>
+            <span class="glyphicon glyphicon-star" ></span>
+            <span class="glyphicon glyphicon-star" ><label style="margin-left:115px;font-size:18px;">$180</label></span>
+          </div>
+          <div class ="col-md-3">
+             <a target="_blank" href="#">
+             <img  src="<?php echo get_template_directory_uri();?>/images/img_1372.jpg"  class="img-thumbnail ">
+             </a>
+             <div class="desc"><p> Awesome  Brown dress for nice and  awesome ladies</p></div>
+             <span class="glyphicon glyphicon-star" style="margin-left:100px;"></span>
+             <span class="glyphicon glyphicon-star" ></span>
+             <span class="glyphicon glyphicon-star" ></span>
+             <span class="glyphicon glyphicon-star" ></span>
+             <span class="glyphicon glyphicon-star" ><label style="margin-left:115px;font-size:18px;">$320</label></span>
+          </div>
+          <div class ="col-md-3">
+               <a target="_blank" href="#">
+               <img src="<?php echo get_template_directory_uri();?>/images/821390.jpg"  class="img-thumbnail ">
+               </a>
+               <div class="desc"><p> Awesome Pink vest for nice and  awesome ladies</p></div>
+               <span class="glyphicon glyphicon-star" style="margin-left:100px;"></span>
+               <span class="glyphicon glyphicon-star" ></span>
+               <span class="glyphicon glyphicon-star" ></span>
+               <span class="glyphicon glyphicon-star" ></span>
+               <span class="glyphicon glyphicon-star" ><label style="margin-left:115px;font-size:18px;">$270</label></span>
+          </div>
+          </div>
+        </div>
+        <!--
+        Sara add some things you can add it or not "it is up to you :D"
+
+<h3>Accessories</h3>
+                 <hr style="margin-left: 80px; width: 1300px;margin-top: 80px;">   
+        <div class="row" id="moredress">
+          <div class ="col-md-3">
+               <a target="_blank" href="#">
+              <img src="<?php echo get_template_directory_uri();?>/images/310.jpg"  class="img-thumbnail ">
+              </a>
+              <div class="desc"><p>Dar Awesome rings  for your awesome fingers</p></div>
+              <span class="glyphicon glyphicon-star" style="margin-left:100px;"></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ><label style="margin-left:80px;font-size:18px;">$750</label></span>
+          </div>
+          <div class ="col-md-3">
+             <a target="_blank" href="#">
+             <img src="<?php echo get_template_directory_uri();?>/images/images-cambnat-3555.jpg"  class="img-thumbnail ">
+             </a>
+             <div class="desc"><p> Awesome accesoriess for nice and  awesome ladies</p></div>
+             <span class="glyphicon glyphicon-star" style="margin-left:100px;"></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ><label style="margin-left:80px;font-size:18px;">$280</label></span>
+          </div>          
+          <div class ="col-md-3">
+             <a target="_blank" href="#">
+             <img  src="<?php echo get_template_directory_uri();?>/images/107668alsh3er.jpg"  class="img-thumbnail ">
+             </a>
+             <div class="desc"><p> Awesome  rings for nice and  awesome ladies</p></div>
+             <span class="glyphicon glyphicon-star" style="margin-left:100px;"></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ><label style="margin-left:80px;font-size:18px;">$320</label></span>
+          </div>            
+            <div class ="col-md-3">
+               <a target="_blank" href="#">
+               <img src="<?php echo get_template_directory_uri();?>/images/images-cambnat-3603.jpg"  class="img-thumbnail ">
+               </a>
+               <div class="desc"><p> Awesome accessories for nice and  awesome ladies</p></div>
+               <span class="glyphicon glyphicon-star" style="margin-left:100px;"></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ></span>
+              <span class="glyphicon glyphicon-star" ><label style="margin-left:80px;font-size:18px;">$470</label></span>
+            </div>          
+            </div>
+        </div>
+            <hr style="margin-top:80px;">
+          <div class="container" id=" container">
+            <div class="row">              
+                 <div class="col-md-8">
+                   <form method='post'>
+                      <label style="margin-top:40px;margin-left:70px;font-size:22px;color:#ff8067;">Comment:</label><br />
+                      <textarea name='comments' id='comments' ></textarea><br /><br/>
+                      <input type='submit' value='Submit' style="margin-left:160px;width:120px;background-color:#ff8067;"/>  
+                   </form>
+                 </div>
+            </div><br/><br/>
+<div class="row">
+  <img src="<?php echo get_template_directory_uri();?>/images/profile-pic.jpg" width="70">
+      <label><a href="#">Sarah Abdelhai</a></label>
+  <div class="dialogbox">
+    <div class="body">
+      <span class="tip tip-up"></span>
+      <div class="message">
+        <span>I buy a nice dress from you and i satisfy about it so thank you for every one in team .</span>
+      </div>
+    </div>
+  </div>
+  <img src="<?php echo get_template_directory_uri();?>/images/profile-pic.jpg" width="70">
+      <label><a href="#">Ahmed Hamed</a></label>
+  <div class="dialogbox">
+    <div class="body">
+      <span class="tip tip-down"></span>
+      <div class="message">
+        <span>This website is so helpful to make product easier to people so thanks !.</span>
+      </div>
+    </div>
+  </div>
+  
+
+  <img src="<?php echo get_template_directory_uri();?>/images/profile-pic.jpg" width="70">
+      <label><a href="#">Hossam Hassan</a></label>
+  <div class="dialogbox">
+    <div class="body">
+      <span class="tip tip-left"></span>
+      <div class="message">
+        <span>I bought a good material T-shirt from this website and i love it more.</span>
+      </div>
+    </div>
+  </div>
+  <img src="<?php echo get_template_directory_uri();?>/images/profile-pic.jpg" width="70">
+      <label><a href="#">Ahmed Fathy</a></label>
+  <div class="dialogbox">
+    <div class="body">
+      <span class="tip tip-right"></span>
+      <div class="message">
+        <span>your products is so good so i wish continue improve that products.</span>
+      </div>
+    </div>
+  </div>
+  </div>
+</div>
+</div> -->
+<?php
+get_footer();
