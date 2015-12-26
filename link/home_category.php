c2f184c45cf0c816e1f59c4321b8ea09e68a58ab
Aya AbuElkhair <ayaabuelkhair93@yahoo,com>
2015-12-26 03:48:43 +0200

    this is a home_category bage but just like php page it works in the same way like any page.php

---
+++ link/home_category.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+ <html lang="en">
+   <head>
+     <meta charset="utf-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1"> 
+     <title>Task</title> 
+ 	<link rel="stylesheet" href="css/bootstrap.min.css">
+ 	<link rel="stylesheet" href="css/bootstrap.css">
+     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+     <link rel="stylesheet" href="css/validationEngine.jquery.css" type="text/css"/>
+ 	<link rel="stylesheet" href="css/template.css" type="text/css"/>
+     <link href="css/bootstrap.min.css" rel="stylesheet">
+     <link href="css/jquery-ui.css" rel="stylesheet">
+ 	<link href="style.css" rel="stylesheet"> 
+     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">	
+   </head>
+   <body>
+<header>
+<nav class=" nav navbars navbar">
+  <div class="navbar-header col-sm-2 soka">
+      SOKA
+  </div>
+<div>
+<ul class="nav nav-tabs colornavul">
+  <li><a href="#home" class="active 1" style="color:black">Home</a></li>
+  <li><a href="#news 2">About</a></li>
+  <li><a href="#about 3">Men</a></li>
+  <li><a href="#about 4">Wamen</a></li>
+  <li><a href="#about 5">Girls</a></li>
+  <li><a href="#about 6">Boys</a></li>
+  <li><a href="#about 7">Blog</a></li>
+  <li><a href="#contact 8">Contact</a></li>
+  <li id="p1">Login</li>
+  <li><span class="fa-stack col-sm-1">
+  <i class="fa fa-circle-thin fa-stack-2x"></i>
+  <i class="fa fa-search fa-stack-1x"></i>
+  </span></li>
+  <a href="http://twitter.com" target="_blanck">
+  <li><span class="fa-stack col-sm-1">
+  <i class="fa fa-circle-thin fa-stack-2x"></i>
+  <i class="fa fa-twitter fa-stack-1x"></i>
+  </span></li></a>
+
+  <a href="http://vimeo.com" target="_blanck">
+  <li><span class="fa-stack col-sm-1">
+  <i class="fa fa-circle-thin fa-stack-2x"></i>
+  <i class="fa fa-vimeo fa-stack-1x"></i>
+  </span></li></a>
+
+  <a href="http://facebook.com" target="_blanck">
+  <li><span class="fa-stack col-sm-1">
+  <i class="fa fa-circle-thin fa-stack-2x"></i>
+  <i class="fa fa-facebook fa-stack-1x "></i>
+  </span></li></a>
+</ul> 
+</div>
+</nav>
+</header>
+ 
+ 
+ 
+  
+ 
+   <!-- category slider -->
+ 
+ <div id="soka-gallery" class="carousel slide  col-lg-12 clear-affix" data-ride="carousel">
+   <div class="carousel-inner  carusal_slide" role="listbox">
+     <div class="item img_gallery active">
+       <img class="img" src="images/slider-1.jpg" alt="">  
+ 	    <img class="btn btn-succes" id="offercart" src="images/offer-cart.png" alt="">
+ 		 <a href="www.google.com" id="hint1"> <img class="btn btn-succes"  src="images/hint1.png" alt=""> </a>
+     </div>
+     <div class="item  img_gallery">
+       <img class="img" src="images/slider-2.jpg" alt="">
+ 	  <a href="www.google.com" id="offercart"><img class="btn btn-succes"  src="images/offer-cart.png" alt=""></a>
+ 	   <a href="www.google.com" id="hint2"> <img class="btn btn-succes"  src="images/hint2.png" alt=""> </a>
+     </div>
+   </div>
+   <div class="arrows">
+    <a class="left " href="#soka-gallery" role="button" data-slide="prev">
+        <img src="images/arrow_left.jpg" alt="" />
+     <span class="sr-only">Previous</span>
+   </a>
+    <a class="right " href="#soka-gallery" role="button" data-slide="next">
+     <img src="images/arrow_right.jpg" alt="" />
+     <span class="sr-only">Next</span>
+   </a>
+   </div>
+   </div>
+   
+     <!-- offers -->
+   <div class="offers">
+    <div class="container">
+     <div class="row">
+       <div class="col-lg-4 col-md-8 col-sm-10 col-xs-12  offer_item">
+ 	     <h2>DONT LET FASHION GO TO WASTE</h2>
+ 		 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+ 		 <h4>soka</h4>
+ 		 <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">Read More</a></div>
+ 	  </div>
+ 	  <div class="col-lg-4 col-md-8 col-sm-10 col-xs-12  offer_item">
+ 	  <h2>DONT LET FASHION GO TO WASTE</h2>
+ 	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+ 		 <h4>soka</h4>
+ 		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">View Collection</a></div>
+ 	  </div>
+ 	  <div class="col-lg-4 col-md-8 col-sm-10 col-xs-12  offer_item">
+ 	  <h2>DONT LET FASHION GO TO WASTE</h2>
+ 	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+ 		 <h4>soka</h4>
+ 		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">View Collection</a></div>
+ 	  </div>
+     </div>
+ 	    <div class="row">
+       <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12  offer_item">
+ 	     <h2>DONT LET FASHION GO TO WASTE</h2>
+ 		 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+ 		 <h4>soka</h4>
+ 		  <div id="read_more" class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">Read More</a></div>
+ 	  </div>
+ 	  <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12 offer_item">
+ 	  <h2>DONT LET FASHION GO TO WASTE</h2>
+ 	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+ 		 <h4>soka</h4>
+ 		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">View Collection</a></div>
+ 	  </div>
+ 	  <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12  offer_item">
+ 	  <h2>DONT LET FASHION GO TO WASTE</h2>
+ 	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+ 		 <h4>soka</h4>
+ 		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">View Collection</a></div>
+ 	  </div>
+     </div>
+ 	    <div class="row">
+       <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12 offer_item">
+ 	     <h2>DONT LET FASHION GO TO WASTE</h2>
+ 		 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+ 		 <h4>soka</h4>
+ 		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">Read More</a></div>
+ 	  </div>
+ 	  <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12 offer_item">
+ 	  <h2>DONT LET FASHION GO TO WASTE</h2>
+ 	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+ 		 <h4>soka</h4>
+ 		  <div class="meta-data"><span class="glyphicon glyphicon-play"></span><a href="#">View Collection</a></div>
+ 	  </div>
+ 	  <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12 offer_item">
+ 	  <h2>DONT LET FASHION GO TO WASTE</h2>
+ 	 <img src="images/offer1.jpg" alt="summer offers from factoria" />
+ 		 <h4>soka</h4>
+ 		 <div class="meta-data"> <span class="glyphicon glyphicon-play"></span><a href="#">View Collection</a> </div>
+ 	  </div>
+     </div>
+    </div>
+   </div>
+
+   
+<footer>
+<div class="container-fluid footer">
+<div  class="row1">
+  <div class="col-lg-4  col1">
+     <h4 class="subtitle">About <span class="tcr">SOKA</span></h4>
+    <h5>
+       He says,Monsieur.that his
+      principles won't admit of his
+      drinking
+    </h5>
+     <h5>
+        But that if Monsieur wants to 
+      live another day to drink,then
+      Monsieur had best drop all. 
+
+    </h5>
+    <h5>
+    </br>
+     Boats,and pull the ship away.
+    </h5>
+  </div>
+  <div class="col-lg-4   col2">
+    <h4 class="subtitle">We tweet.<span class="tcr">ALOT</span></h4>
+     <span class="fa fa-twitter fa1"></span>
+    <h5>
+    He says,Mansieur,that his 
+    principles won't work and 
+      don't.
+      </h5>
+     <span class="fa fa-twitter fa2"></span>
+    <h5 class="rr">#Andrwe,Yesterday</h5>
+    <h5>
+     He says,Mansieur,that his 
+     principles won't work.  
+     </h5>
+     <h5 class="rr">#Andrwe,Yesterday</h5>
+  </div>
+  <div class="col-lg-4  col3">
+     <h4 class="subtitle">Here we are.On<span class="tcr"> FLICKR</span></h4>
+   <div class="row">
+   <div class="col-lg-4"><img src="images/footer-1.jpg" alt="" /></div>
+   <div class="col-lg-4"><img src="images/footer-4.jpg" alt="" /></div>
+   <div class="col-lg-4"><img src="images/footer-9.jpg" alt="" /></div>
+   </div>
+   <div class="row">
+   <div class="col-lg-4"><img src="images/footer-2.jpg" alt="" /></div>
+   <div class="col-lg-4"><img src="images/footer-5.jpg" alt="" /></div>
+   <div class="col-lg-4"><img src="images/footer-8.jpg" alt="" /></div>
+   </div>
+   <div class="row">
+   <div class="col-lg-4"><img src="images/footer-3.jpg" alt="" /></div>
+   <div class="col-lg-4"><img src="images/footer-6.jpg" alt="" /></div>
+   <div class="col-lg-4"><img src="images/footer-7.jpg" alt="" /></div>
+   </div>
+  </div>
+</div>
+   <div id="footer" class="col-sm-12">
+      <div class="fdiv col-sm-6">
+        Copyright <span class="glyphicon glyphicon-copyright-mark"></span> 2013 SOKA 2012.All right reserved
+      </div>
+      <div class="fdiv1 col-sm-6">
+        Developed by SOKA Team
+      </div>
+   </div>
+</footer>
+
+  
+     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+ 		<script src="js/jquery-1.11.3.min.js"></script> 
+ 	    <script src="js/jquery-1.8.2.min.js" type="text/javascript"></script>
+ 		<script src="js/main.js"></script>
+ 		<script src="js/bootstrap.js"></script> 
+ 		<script src="js/jquery-ui.js" ></script>
+ 		<script src="js/languages/jquery.validationEngine-en.js" type="text/javascript" charset="utf-8"></script>
+ 	    <script src="js/jquery.validationEngine.js" type="text/javascript" charset="utf-8"></script>
+   </body>
+ </html> 
---
+++ link/js/main.js
@@ -0,0 +1,19 @@
+function changeColor(){
+               
+               var xBox=document.getElementById("box");
+
+                xBox.style.backgroundColor=document.getElementById("vColor").value; 
+}
+
+  $(".offer_item ").hover(function(){
+	  
+  $(this).find(".meta-data").show(500);
+  $(this).find("img").fadeTo(200,.5);
+
+});
+  $(".offer_item").mouseleave(function(){
+	  
+  $(this).find(".meta-data").hide(500);
+  $(this).find("img").fadeTo(50,10);
+});
+
